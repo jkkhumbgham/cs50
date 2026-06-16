ac64c6cbf05eb09315ac3578fc89cef05a2c9837
jkkhumbgham <juanalbac06@gmail.com>
2026-06-16 13:43:47 -0500

    documentacion mario
    Added loops to print a pyramid pattern based on user input height.

--- Problem_set_1/mario.cpp
+++ Problem_set_1/mario.cpp
@@ -1,19 +1,22 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 int main() {
-    
+    //pedir y almacenar la altura al usuario
     cout << "Altura:"<<endl;
     int altura;
     cin >> altura;
+    //loops para las filas, mismo tamaño que la altura dada por el usuario
     for(int filas = 1; filas <=altura; filas++){
+        //loop que imprime los espacios a la izquierda de los hashes
         for(int spaces=0;spaces<altura-filas;spaces++){
             cout<<" ";
         }
+        //loop que imprime los primeros hashes luego de los espacios
         for(int hash1=0;hash1<filas;hash1++){
             cout<<"#";
         }
         cout<<"  ";
+        //loop que imprime los hashes luego de el doble espacio para completar la piramide
         for(int hash2=0;hash2<filas;hash2++){
             cout<<"#";
         }
